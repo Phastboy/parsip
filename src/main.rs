eda6ae342593179319a28f57d6794341a0814990
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-08-08 18:58:42 +0100

    Wire ParsipServer and connection accept loop into main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod listener;
+use listener::{ParsipServer, accept};
+
+fn main() -> Result<(), std::io::Error> {
+    let address = "0.0.0.0:9000";
+    let server = ParsipServer::new(address);
+    let listener = server.listen()?;
+
+    println!("Listening on {}", address);
+    println!("Waiting for connection...");
+
+    let (_stream, peer_address) = accept(&listener)?;
+
+    println!("Connection accepted from {}", peer_address);
+
+    Ok(())
 }
