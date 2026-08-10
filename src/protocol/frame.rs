de73275e88e2e0e9fc2f2d61c42ca324ccd256f3
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-08-10 09:16:12 +0100

    refactor: modularize protocol and implement manual serialization

---
+++ src/protocol/frame.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Clone)]
+pub struct Frame {
+    pub message_type: u8,
+    pub payload: Vec<u8>,
+}
+
+impl Frame {
+    pub fn new(message_type: u8, payload: Vec<u8>) -> Self {
+        Self { message_type, payload }
+    }
+}
