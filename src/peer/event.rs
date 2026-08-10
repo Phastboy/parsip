f9b8a9d8a1bf1d9c28d3e84f995b1dc966191d75
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-08-10 09:16:12 +0100

    refactor: split peer module and fix race condition in connection registration

---
+++ src/peer/event.rs
@@ -0,0 +1,10 @@
+use std::net::SocketAddr;
+use crate::identity::PeerId;
+use crate::protocol::Message;
+
+pub enum PeerEvent {
+    NewConnection(PeerId),
+    Disconnected(PeerId),
+    Message(PeerId, Message),
+    Discovered(PeerId, SocketAddr),
+}
